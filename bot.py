2bb51f862fbfa309ff5efe492fa79863f74abc22
“namelew” <diogomaciel.cunha@gmail.com>
2021-12-08 08:09:31 -0300

    criando primeiro script para bot do discord

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+#criar arquivo .env com ambos os dados para acessar as diretivas
+token = os.getenv('DISCORD_TOKEN') # tolken de auth
+guild = os.getenv('DISCORD_GUILD') # nome do serve
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for g in client.guilds:
+        if g.name == guild:
+            break
+    print(f'{client.user} has connected to the follow guild:\n{g.name}(id:{g.id})')
+
+client.run(token)
